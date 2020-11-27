f48c05bf90643bc1c372c65715faa83929f761c3
smallville7123 <smallville7123@gmail.com>
2020-11-27 22:41:09 +1000

    fix issue where delay was being added to wrong channel

--- AAudioTrack2/src/main/cpp/smallville7123/plugins/Delay.h
+++ AAudioTrack2/src/main/cpp/smallville7123/plugins/Delay.h
@@ -23,20 +23,9 @@ public:
      * return true if we still have data to write, otherwise false
      */
     bool write(PortUtils2 * in, PortUtils2 * out, unsigned int samples) {
-        //
-        // called as delay.write(mixerPortA, tmpPort);
-        //
-        // the value of samples is dependant on the audio engine, but is always equal to the samples
-        // remaining in the audio block
-        // it is one of two values:
-        //
-        // value 1: mixerPortA->ports.samples - i
-        // value 2: mixerPortA->ports.samples
-        //
-
         for (int i = 0; i < out->ports.samples; i += 2) {
-            reinterpret_cast<ENGINE_FORMAT *>(out->ports.outputStereo->l->buf)[i] = right.delayline(reinterpret_cast<ENGINE_FORMAT *>(in->ports.outputStereo->l->buf)[i]);
-            reinterpret_cast<ENGINE_FORMAT *>(out->ports.outputStereo->r->buf)[i] = left.delayline(reinterpret_cast<ENGINE_FORMAT *>(in->ports.outputStereo->r->buf)[i]);
+            reinterpret_cast<ENGINE_FORMAT *>(out->ports.outputStereo->l->buf)[i] = left.delayline(reinterpret_cast<ENGINE_FORMAT *>(in->ports.outputStereo->l->buf)[i]);
+            reinterpret_cast<ENGINE_FORMAT *>(out->ports.outputStereo->r->buf)[i] = right.delayline(reinterpret_cast<ENGINE_FORMAT *>(in->ports.outputStereo->r->buf)[i]);
         }
         return true;
     }
